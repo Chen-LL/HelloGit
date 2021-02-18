5734ad1ee499ee66a145b0f5ef1c125791c8cee4
kunliang <liangzaia321@qq.com>
2021-02-18 16:02:10 +0800

    2222222

--- HelloGit/src/main/java/org/example/git/HelloGit.java
+++ HelloGit/src/main/java/org/example/git/HelloGit.java
@@ -9,5 +9,6 @@ package org.example.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("222222222");
     }
 }
