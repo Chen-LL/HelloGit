6e928ec719cd13bc51502102dd8325efa50a9e79
kunliang <liangzaia321@qq.com>
2021-02-18 15:50:59 +0800

    idea创建文件

---
+++ HelloGit/src/main/java/org/example/git/HelloGit.java
@@ -0,0 +1,13 @@
+package org.example.git;
+
+/**
+ * @author kunliang
+ * @version 1.0
+ * @date 2021/2/18 15:43
+ * @description
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
