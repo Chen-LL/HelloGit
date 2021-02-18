e61b45ec99113199ea6c0afdc5c440ba5253d845
Chen-LL <63381087+Chen-LL@users.noreply.github.com>
2021-02-18 16:05:42 +0800

    Update HelloGit.java

--- HelloGit/src/main/java/org/example/git/HelloGit.java
+++ HelloGit/src/main/java/org/example/git/HelloGit.java
@@ -11,5 +11,6 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("222222222");
         System.out.println("3333333333");
+        System.out.println("44444444");
     }
 }
